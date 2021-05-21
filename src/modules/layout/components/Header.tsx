c8645e1d738f295b76045000997ecaad691e4591
Natacha De la Rosa <delarosa.natacha@gmail.com>
2021-05-21 09:22:51 -0400

    fix(addresses): update contract addresses to test group

--- src/modules/layout/components/Header.tsx
+++ src/modules/layout/components/Header.tsx
@@ -1,24 +1,11 @@
 import { FC } from "react";
-import {
-  StackProps,
-  HStack,
-  Text,
-  useColorMode,
-  IconButton,
-  Icon,
-  Link,
-  Box,
-  Heading,
-} from "@chakra-ui/react";
+import { StackProps, HStack, Link, Box, Heading } from "@chakra-ui/react";
 import { Link as ReachLink } from "@reach/router";
 
 // common
-import { useUserInfo } from "modules/common/hooks/useUserInfo";
 import { ROUTES } from "modules/common/lib/routes";
 
 const Header: FC<StackProps> = ({ ...stackProps }) => {
-  const { userIsAdmin, userIsManager } = useUserInfo();
-
   return (
     <HStack
       px="2rem"
@@ -37,35 +24,19 @@ const Header: FC<StackProps> = ({ ...stackProps }) => {
         </Link>
       </Box>
       <HStack spacing={4}>
-        {userIsAdmin && (
-          <>
-            <Link
-              _hover={{ textDecor: "none" }}
-              as={ReachLink}
-              to={ROUTES.roles}
-            >
-              <Heading size="sm">Roles</Heading>
-            </Link>
-            <Link
-              _hover={{ textDecor: "none" }}
-              as={ReachLink}
-              to={ROUTES.management}
-            >
-              <Heading size="sm">Manage</Heading>
-            </Link>
-          </>
-        )}
-        {userIsManager && (
-          <>
-            <Link
-              _hover={{ textDecor: "none" }}
-              as={ReachLink}
-              to={ROUTES.manager}
-            >
-              <Heading size="sm">Payments</Heading>
-            </Link>
-          </>
-        )}
+        <Link _hover={{ textDecor: "none" }} as={ReachLink} to={ROUTES.roles}>
+          <Heading size="sm">Roles</Heading>
+        </Link>
+        <Link
+          _hover={{ textDecor: "none" }}
+          as={ReachLink}
+          to={ROUTES.management}
+        >
+          <Heading size="sm">Manage</Heading>
+        </Link>
+        <Link _hover={{ textDecor: "none" }} as={ReachLink} to={ROUTES.manager}>
+          <Heading size="sm">Payments</Heading>
+        </Link>
       </HStack>
     </HStack>
   );
